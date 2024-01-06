9568f35a94ffff6cad869fa04eb5653b44d26f00
sooheyon <sooheyonjo@gmail.com>
2024-01-06 10:33:26 +0900

    sign in sign out test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
-import { NextPage } from "next"
+'use client'
 
-const Home: NextPage = ()=>{
-  return <div className="bg-red-100">Hello, NextJS</div>
-}
+import { NextPage } from "next";
+import { signIn,signOut,  useSession } from "next-auth/react";
+import { useEffect } from "react";
 
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  const onClickSignIn = ()=>{
+    signIn()
+  }
+
+  const onClickSignOut = ()=>{
+    signOut()
+  }
+
+  useEffect(()=>{
+    console.log(session)
+  },[session])
+
+  if(!session) {
+    return <div>
+      <button onClick={onClickSignIn}>Sign In</button>
+    </div>
+  }
+
+
+  return <div className="bg-red-100">
+    <button onClick={onClickSignOut}>Sign out</button>
+  </div>;
+};
 
 export default Home;
