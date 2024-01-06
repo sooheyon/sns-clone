49ae897ce66ac28f618a51d13732675b5cd7df52
sooheyon <sooheyonjo@gmail.com>
2024-01-06 10:10:22 +0900

    create next auth provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,13 @@
-import NextAuth from 'next-auth/next'
+import NextAuth from "next-auth/next";
+import Github from "next-auth/providers/github";
 
 const handler = NextAuth({
-  providers:[],
-})
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
 
-export  {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
