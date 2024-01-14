c5fff638f79bdc4da937c36b2e02e522e31b1ed2
sooheyon <sooheyonjo@gmail.com>
2024-01-14 10:10:33 +0900

    prisma session setting

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth/next";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+
+const prismaClient = new PrismaClient()
 
 const handler = NextAuth({
   providers: [
@@ -13,7 +17,8 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
-  
+  adapter: PrismaAdapter(prismaClient)
+
 });
 
 export { handler as GET, handler as POST };
