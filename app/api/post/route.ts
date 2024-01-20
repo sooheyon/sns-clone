db4402e759515d0251cc0b545e1853a59a12fc1e
sooheyon <sooheyonjo@gmail.com>
2024-01-20 12:59:38 +0900

    create post backend

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,8 +1,55 @@
+import { authOptions } from "@/app/lib/auth";
+import { prismaClient } from "@/app/lib/prismaClient";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 //http://localhost:3000/api/post
 export const POST = async (request: NextRequest) => {
-  return NextResponse.json({
-    message: "Hello",
-  });
+  try {
+    const { content } = await request.json();
+    const session = await getServerSession(authOptions);
+
+    if (!content) {
+      return NextResponse.json(
+        {
+          message: "Not exist content",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Not exist session",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const post = await prismaClient.post.create({
+      data: {
+        content,
+        userId: session.user.id,
+      },
+    });
+
+    return NextResponse.json(post);
+    
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message: error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 };
