4fde19b6fe42e29b3a24ae49d795faa135d5a364
sooheyon <sooheyonjo@gmail.com>
2024-03-03 10:48:38 +0900

    대댓글 생성 기능 추가

--- app/components/CommentCard.tsx
+++ app/components/CommentCard.tsx
@@ -1,28 +1,69 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
 import { formatDistanceToNow } from "date-fns";
 import { ko } from "date-fns/locale";
 import { IComment } from "@/types";
+import CreateCommentForm from "./CreateCommentForm";
+import axios from "axios";
 
 interface CommentCardProps {
   comment: IComment;
 }
 
 const CommentCard: FC<CommentCardProps> = ({ comment }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [content, setContent] = useState<string>("");
+
+  console.log('[comment]',comment)
+
+  const onSubmitCreateComment = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+
+      if (!content) return;
+
+      const response = await axios.post<IComment>(
+        `${process.env.NEXT_PUBLIC_URL}/api/comment/${comment.id}`,
+        {
+          content,
+        }
+      );
+
+      setContent("");
+      setIsOpen(false);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <li className="flex justify-between gap-2">
-      <div>
-        <span className="font-bold">{comment.user.name}</span>
-        <span className="ml-1">
-          #{comment.userId.substring(comment.userId.length - 4)}
-        </span>
-        <span className="ml-2">{comment.content}</span>
-      </div>
-      <div>
-        <span className="ml-2">
-          {formatDistanceToNow(new Date(comment.createdAt), { locale: ko })}
-        </span>
-      </div>
-    </li>
+    <>
+      <li
+        className="flex justify-between gap-2 cursor-pointer"
+        onClick={() => setIsOpen(true)}
+      >
+        <div>
+          <span className="font-bold">{comment.user.name}</span>
+          <span className="ml-1">
+            #{comment.userId.substring(comment.userId.length - 4)}
+          </span>
+          <span className="ml-2">{comment.content}</span>
+        </div>
+        <div>
+          <span className="ml-2">
+            {formatDistanceToNow(new Date(comment.createdAt), { locale: ko })}
+          </span>
+        </div>
+      </li>
+
+      {isOpen && (
+        <CreateCommentForm
+          onSubmitCreateComment={onSubmitCreateComment}
+          content={content}
+          setContent={setContent}
+        />
+      )}
+    </>
   );
 };
 
